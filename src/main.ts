3539f5ac131526704f0163cbef95185ccbc83a89
Langbein, Michael <michael.langbein@dlr.de>
2020-05-29 17:50:06 +0200

    painting two triangles with different colors

--- src/main.ts
+++ src/main.ts
@@ -12,19 +12,22 @@ const program = initShaderProgram(gl, vertexShaderSource, fragmentShaderSource);
 setup3dScene(gl, program);
 
 const posBuffer = createFloatBuffer(gl, [
+    // triangle one
     [-.8, .8],
-    [.8, -.1],
-    [-.1, -.8]
+    [.1, -.6],
+    [-.5, -.8],
+
+    // triangle two
+    [-.2, .8],
+    [.8, .8],
+    [.9, -.6]
 ]);
 const posAttributeLocation = getAttributeLocation(gl, program, 'aVertexPosition');
 bindBufferToAttribute(gl, posAttributeLocation, posBuffer);
 
-
 const colorUniformLocation = getUniformLocation(gl, program, 'uColor');
 bindValueToUniform(gl, colorUniformLocation, '4f', [.1, .1, .9, .9]);
 
-
-var offset = 0;
-gl.drawArrays(gl.TRIANGLES, offset, posBuffer.vectorCount);
-
-
+gl.drawArrays(gl.TRIANGLES, 0, 3);
+bindValueToUniform(gl, colorUniformLocation, '4f', [.9, .1, .1, .9]);
+gl.drawArrays(gl.TRIANGLES, 3, 3);
