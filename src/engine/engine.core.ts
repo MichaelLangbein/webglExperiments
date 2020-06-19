b93026e61dbb73bf5cb3c54e41d9c6cb28b1eac1
Langbein, Michael <michael.langbein@dlr.de>
2020-06-19 15:16:16 +0200

    feature: created shader-object, that handles binding of parameters and rendering and does some degree of sanity checking

--- src/engine/engine.core.ts
+++ src/engine/engine.core.ts
@@ -1,10 +1,12 @@
-import { createShaderProgram, setup3dScene, createFloatBuffer, getAttributeLocation, bindBufferToAttribute, getUniformLocation, bindValueToUniform, clearBackground, BufferObject, UniformType, bindProgram, createTexture, bindTextureToUniform, TextureObject } from './webgl';
+import { createShaderProgram, setup3dScene, createFloatBuffer, getAttributeLocation, bindBufferToAttribute, getUniformLocation, bindValueToUniform, clearBackground, BufferObject, UniformType, bindProgram, createTexture, bindTextureToUniform, TextureObject, FramebufferObject, bindFramebuffer, bindOutputCanvasToFramebuffer, updateBufferData } from './webgl';
 const hash = require('string-hash');
 
 
 export interface IProgram {
     program: WebGLProgram;
     id: string;
+    vertexShaderSource: string;
+    fragmentShaderSource: string;
 }
 
 
@@ -13,7 +15,9 @@ export class Program implements IProgram {
     readonly program: WebGLProgram;
     readonly id: string;
 
-    constructor(gl: WebGLRenderingContext, vertexShaderSource: string, fragmentShaderSource: string) {
+    constructor(gl: WebGLRenderingContext,
+        readonly vertexShaderSource: string,
+        readonly fragmentShaderSource: string) {
         this.program = createShaderProgram(gl, vertexShaderSource, fragmentShaderSource);
         this.id = hash(vertexShaderSource + fragmentShaderSource);
     }
@@ -24,6 +28,7 @@ export interface IUniform {
     location: WebGLUniformLocation;
     type: UniformType;
     value: number[];
+    variableName: string;
 }
 
 
@@ -32,11 +37,13 @@ export class Uniform implements IUniform {
     readonly location: WebGLUniformLocation;
     readonly type: UniformType;
     readonly value: number[];
+    readonly variableName: string;
 
     constructor(gl: WebGLRenderingContext, program: IProgram, variableName: string, type: UniformType, data: number[]) {
         this.location = getUniformLocation(gl, program.program, variableName);
         this.type = type;
         this.value = data;
+        this.variableName = variableName;
     }
 }
 
@@ -45,6 +52,7 @@ export interface ITexture {
     location: WebGLUniformLocation;
     bindPoint: number;
     texture: TextureObject;
+    variableName: string;
 }
 
 export class Texture implements ITexture {
@@ -52,11 +60,13 @@ export class Texture implements ITexture {
     readonly location: WebGLUniformLocation;
     readonly bindPoint: number;
     readonly texture: TextureObject;
+    readonly variableName: string;
 
     constructor(gl: WebGLRenderingContext, program: IProgram, variableName: string, image: HTMLImageElement, bindPoint: number) {
         this.location = getUniformLocation(gl, program.program, variableName);
         this.texture = createTexture(gl, image);
         this.bindPoint = bindPoint;
+        this.variableName = variableName;
     }
 }
 
@@ -64,6 +74,7 @@ export class Texture implements ITexture {
 export interface IAttribute {
     location: number;
     value: BufferObject;
+    variableName: string;
 }
 
 
@@ -71,12 +82,132 @@ export class Attribute implements IAttribute {
 
     readonly location: number;
     readonly value: BufferObject;
+    readonly variableName: string;
 
     constructor(gl: WebGLRenderingContext, program: IProgram, variableName: string, data: number[][]) {
         this.location = getAttributeLocation(gl, program.program, variableName);
         this.value = createFloatBuffer(gl, data);
+        this.variableName = variableName;
     }
 }
+
+
+
+function first<T>(arr: T[], condition: (el: T) => boolean): T {
+    for (const el of arr) {
+        if (condition(el)) {
+            return el;
+        }
+    }
+}
+
+
+function parseProgram(program: IProgram): [string[], string[], string[]] {
+    const attributeRegex = /^attribute (int|float|vec2|vec3|vec4|mat2|mat3|mat4) (\w*);/gm;
+    const uniformRegex = /^uniform (int|float|vec2|vec3|vec4|mat2|mat3|mat4) (\w*);/gm;
+    const textureRegex = /^uniform sampler2D (\w*);/gm;
+
+    const shaderCode = program.fragmentShaderSource + '\n\n\n' + program.vertexShaderSource;
+
+    const attributeNames = [];
+    const attributeMatches = attributeRegex.exec(shaderCode);
+    for (const attrMatch in attributeMatches) {
+        attributeNames.push(attrMatch[2]);
+    }
+    const uniformNames = [];
+    const uniformMatches = uniformRegex.exec(shaderCode);
+    for (const uniformMatch in uniformMatches) {
+        uniformNames.push(uniformMatch[2]);
+    }
+    const textureNames = [];
+    const textureMatches = textureRegex.exec(shaderCode);
+    for (const textureMatch in textureMatches) {
+        textureNames.push(textureMatch[2]);
+    }
+
+    return [attributeNames, uniformNames, textureNames];
+}
+
+
+
+interface IShader {
+    program: IProgram;
+    attributes: IAttribute[];
+    uniforms: IUniform[];
+    textures: ITexture[];
+    bind: (gl: WebGLRenderingContext) => void;
+    render: (gl: WebGLRenderingContext, frameBuffer?: FramebufferObject) => void;
+    updateAttributeData: (gl: WebGLRenderingContext, variableName: string, newData: number[][]) => void;
+    updateUniformData: (gl: WebGLRenderingContext, variableName: string, newData: number[]) => void;
+}
+
+export class Shader implements IShader {
+    constructor(
+        readonly program: IProgram,
+        readonly attributes: IAttribute[],
+        readonly uniforms: IUniform[],
+        readonly textures: ITexture[]
+    ) {
+        const [attributeNames, uniformNames, textureNames] = parseProgram(program);
+        for (const attrName in attributeNames) {
+            const found = attributes.filter(a => a.variableName === attrName);
+            if (found.length !== 1) {
+                throw new Error(`Provided ${found.length} values for shader's attribute ${attrName}.`);
+            }
+        }
+        for (const uniformName in uniformNames) {
+            const found = uniforms.filter(a => a.variableName === uniformName);
+            if (found.length !== 1) {
+                throw new Error(`Provided ${found.length} values for shader's uniform ${uniformName}.`);
+            }
+        }
+        for (const texName in textureNames) {
+            const found = textures.filter(a => a.variableName === texName);
+            if (found.length !== 1) {
+                throw new Error(`Provided ${found.length} values for shader's texture ${texName}.`);
+            }
+        }
+    }
+
+    public bind(gl: WebGLRenderingContext): void {
+        bindProgram(gl, this.program.program);
+        for (const a of this.attributes) {
+            bindBufferToAttribute(gl, a.location, a.value);
+        }
+        for (const u of this.uniforms) {
+            bindValueToUniform(gl, u.location, u.type, u.value);
+        }
+        for (const t of this.textures) {
+            bindTextureToUniform(gl, t.texture.texture, t.bindPoint, t.location);
+        }
+    }
+
+    public render(gl: WebGLRenderingContext, frameBuffer?: FramebufferObject): void {
+        if (!frameBuffer) {
+            bindOutputCanvasToFramebuffer(gl);
+        } else {
+            bindFramebuffer(gl, frameBuffer);
+        }
+        gl.drawArrays(gl.TRIANGLES, 0, this.attributes[0].value.vectorCount);
+    }
+
+    public updateAttributeData(gl: WebGLRenderingContext, variableName: string, newData: number[][]): void {
+        const attribute = first<IAttribute>(this.attributes, el => el.variableName === variableName);
+        updateBufferData(gl, attribute.value, newData);
+    }
+
+    public updateUniformData(gl: WebGLRenderingContext, variableName: string, newData: number[]): void {
+        const uniform = first<IUniform>(this.uniforms, el => el.variableName === variableName);
+        bindValueToUniform(gl, uniform.location, uniform.type, newData);
+    }
+}
+
+
+
+
+
+
+
 
 
 interface IEntity {
