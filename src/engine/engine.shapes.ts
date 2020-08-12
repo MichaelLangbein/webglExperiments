0aad7660ae30a50d27d7b4e208c5c6932801301e
Langbein, Michael <michael.langbein@dlr.de>
2020-08-12 21:54:18 +0200

    added bezier shapes

--- src/engine/engine.shapes.ts
+++ src/engine/engine.shapes.ts
@@ -1,8 +1,47 @@
 export type Vec2 = [number, number];
 export type Vec3 = [number, number, number];
+export type Vector = number[];
 export type V2Matrix = Vec2[];
 export type V3Matrix = Vec3[];
 export type Matrix = number[][];
+
+
+export function scalarProduct(scalar: number, vector: Vector): Vector {
+    return vector.map(el => scalar * el);
+}
+
+export function vectorAddition(vec0: Vector, vec1: Vector): Vector {
+    if (vec0.length !== vec1.length) {
+        throw new Error('');
+    }
+    const newVec = [];
+    for (let i = 0; i < vec0.length; i++) {
+        newVec.push(vec0[i] + vec1[i]);
+    }
+    return newVec;
+}
+
+export function bezier(p0: Vector, p1: Vector, t: number): Vector {
+    return vectorAddition(scalarProduct( (1.0 - t), p0),  scalarProduct(t, p1));
+}
+
+export function multiBezier(ps: Vector[], t: number): Vector {
+    const length = ps.length;
+    if (length === 2) {
+        return bezier(ps[0], ps[1], t);
+    } else {
+        const vec0 = multiBezier(ps.slice(0, length - 1), t);
+        const vec1 = multiBezier(ps.slice(1, length), t);
+        return bezier(vec0, vec1, t);
+    }
+}
+
+export function bezierGenerator(ps: Vector[]): (t: number) => Vector {
+    return (t: number) => {
+        return multiBezier(ps, t);
+    };
+}
+
 
 
 
